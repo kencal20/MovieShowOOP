40ed6afe17a18142c4a51bba28272e4bcd1ffcf8
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-08-28 11:01:29 +0000

    Accessing static function static from non static function

--- class/movie.class.php
+++ class/movie.class.php
@@ -55,9 +55,17 @@ echo $m->getMovieDetails().'<br/>';
 
 //Accessing static methods and properties
 echo Movies::$voteAge.'<br>';
+
 Movies::setVoteAge(20).'<br/>';
 
 //Accessing static function static from non static function
 echo Movies::$voteAge;
 Movies::setVoteAge(20).'<br/>';
+
+Movies::setVoteAge(20);
+
+//Accessing static function static from non static function
+echo Movies::$voteAge.'<br/>';
+Movies::setVoteAge(20);
+
 $m->getVoteAge();
