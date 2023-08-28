99c76a6839f53008ee3d9ed7cc99900e5cd7b603
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-08-28 12:13:01 +0000

    how to get information from another class

--- class/class.show.php
+++ class/class.show.php
@@ -1,20 +1,30 @@
 
 <?php
-include 'movies.class.php';
+include 'class.movie.php';
 //inherit movie class 
 class Show extends Movies
 {
-
-    public $show_Id;
+    public $show_Id = "ASDC";
     public $venue;
     public $dateTime;
 
     public function display_shows()
     {
-        return $this->Title.''.$this->show_Id;
+        return $this->Title . '' . $this->show_Id;
     }
 
+    public function Movie_showDetails($id,$venue,$dt)
+    {
+        $this->show_Id =$id;
+        $this->venue=$venue;
+        $this->dateTime=$dt;
+
+        return '<br/>'.$this->getMovieDetails().' '.$this->show_Id.' '.$this->venue.' '.$this->$dt;
+    }
 }
+$sh = new Show('123', 'DBC');
+echo $sh->display_shows();
+echo $sh->Movie_showDetails(001,'GTA','20-10-2012').'<br/>';
 
 
 ?>
