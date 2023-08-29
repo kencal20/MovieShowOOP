2935e5dbbe3da90f1bce338a72888ea19734b8da
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-08-29 09:54:05 +0000

    add datereg

--- class/class.customer.php
+++ class/class.customer.php
@@ -4,11 +4,12 @@ include 'class.config.php';
 
 class Customer extends config
 {
+    public $datereg;
     public function add_customer($Name, $DoB, $Addr)
     {
         $sql = 'insert into Customer(FullName,DOB,Address) values(?,?,?)';
         $query = $this->connectdb()->prepare($sql);
-        $query->execute([$Name,$DoB, $Addr]);
+        $query->execute([$Name, $DoB, $Addr]);
         $Customer = $query->fetchAll();
     }
 }
